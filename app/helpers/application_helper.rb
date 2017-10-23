852d19f9809c26e1add3f1d818a6185a08a7f042
kadenjenson <sobeit99@gmail.com>
2017-10-23 16:31:25 -0600

    Implemented rails tag to auto generate html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def login_helper
-  if current_user.is_a?(User)
+    if current_user.is_a?(User)
        link_to "Logout", destroy_user_session_path, method: :delete
     else
        (link_to "Sign up", new_user_registration_path) +
@@ -8,4 +8,12 @@ module ApplicationHelper
        (link_to "Login", new_user_session_path)
     end
   end
+
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout."
+      content_tag(:p, greeting, class: "source_greeting")
+    end
+  end
+
 end
