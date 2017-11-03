e6edfdea1727b38a64519bd86f060329449e6a7f
kadenjenson <sobeit99@gmail.com>
2017-11-03 16:38:19 -0600

    Completed comment feature with action cable

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,8 @@
 module BlogsHelper
+
+  # this is how you can implement a persons profile picture to comments using gravatar
+
+  # def gravatar_helper user
+  #   image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 40
+  # end
 end
